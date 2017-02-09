d2bad69ece9f7f46c5a8b616bceca2c6a73c4731
David <david.gurquiza@gmail.com>
2017-02-09 12:14:36 -0800

    Support x-troxy-host header

--- lib/troxy/proxy.ex
+++ lib/troxy/proxy.ex
@@ -1,13 +1,27 @@
 defmodule Troxy.Proxy do
   use Plug.Builder
+  plug :rewrite_troxy_host_header
   plug :is_alive
   use Troxy.Interfaces.Plug
+
+  def rewrite_troxy_host_header(conn, _opts) do
+    case Plug.Conn.get_req_header(conn, "x-troxy-host") do
+      [] ->
+        conn
+      [target_host] ->
+        # Added the peer to temporarily allow requests from the ui
+        %Plug.Conn{conn | host: target_host, peer: {{127,0,0,2}, 111317}}
+        |> delete_req_header("x-troxy-host")
+        |> delete_req_header("host")
+        |> put_req_header("host", target_host)
+    end
+  end
 
   def is_alive(conn, opts) do
     if File.exists?("partition.lock") do
       conn = conn
       |> put_private(:plug_skip_troxy, true)
-      |> resp(502, "bad")
+      |> resp(502, "bad proxy")
       |> halt
     end
     conn
